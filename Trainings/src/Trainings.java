e4140a17b9088bbfb7502359a6e474ec947942a4
Admin <Admin@DESKTOP-8P8N24S>
2023-08-01 19:56:35 +0200

    first commit

---
+++ Trainings/src/Trainings.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Trainings {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float temperature;
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("Enter temperature in Fahrenheit: ");
+		temperature = scanner.nextInt();
+		
+		temperature = ((temperature-32)*5)/9;
+		System.out.println("Temperature in Celsius: " + temperature);
+
+	}
+
+}
