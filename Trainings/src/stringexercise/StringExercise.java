0bc812dd099457139d56d062794556e0df520084
Admin <Admin@DESKTOP-8P8N24S>
2023-08-12 09:45:21 +0200

    lower case and hashCde/equals

--- Trainings/src/stringexercise/StringExercise.java
+++ Trainings/src/stringexercise/StringExercise.java
@@ -5,7 +5,14 @@ public class StringExercise {
 	public static void main(String args[]) {
 		String string1 = "hello";
 		String string2 = "hemdfhlo";
+		
 		System.out.println(string1.compareTo(string2));
+		System.out.println(string1.endsWith("lo"));
+		
+		System.out.println(string1.hashCode());
+		System.out.println(string1.equals(string2));
+		
+		System.out.println(string1.toLowerCase());
 	}
 
 }
