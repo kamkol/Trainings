65eb3a0647c206e4ea03c6193546ebcfbf90da05
Admin <Admin@DESKTOP-8P8N24S>
2023-08-12 09:42:31 +0200

    ex8 string uses

---
+++ Trainings/src/stringexercise/StringExercise.java
@@ -0,0 +1,11 @@
+package stringexercise;
+
+public class StringExercise {
+	
+	public static void main(String args[]) {
+		String string1 = "hello";
+		String string2 = "hemdfhlo";
+		System.out.println(string1.compareTo(string2));
+	}
+
+}
