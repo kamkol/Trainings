da7e0d61f8c6b12f446338813b7944587fe82a1a
Admin <Admin@DESKTOP-8P8N24S>
2023-08-17 16:40:23 +0200

    ex9 generate random numbers

---
+++ Trainings/src/randomnumber/GenerateRandomNumber.java
@@ -0,0 +1,19 @@
+package randomnumber;
+
+import java.util.Random;
+
+public class GenerateRandomNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x;
+		Random random = new Random();
+		
+		for (x = 1; x<10; x++) {
+			System.out.println(random.nextInt(20));
+			
+		}
+
+	}
+
+}
